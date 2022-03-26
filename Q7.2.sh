fa17d4e9b335f067d301467543f1ceffe8a1b3d4
sarveshped30 <102165590+sarveshped30@users.noreply.github.com>
2022-03-27 00:49:17 +0530

    function for palindrome Number

---
+++ Q7.2.sh
@@ -0,0 +1,24 @@
+is_palindrome ()
+{
+    number=$1
+    copy_of_number=$number
+    reverse_number=0
+    while (( number > 0 ))
+    do
+        remainder=$(( number % 10))
+        reverse_number=$(( reverse_number * 10 + remainder ))
+        (( number /= 10 ))
+
+    done
+    if(( reverse_number == copy_of_number))
+    then
+        echo "$copy_of_number is palindrome"
+    else
+        echo "$copy_of_number is not palindrome"
+    fi
+}
+
+read -p "Enter number:" n
+is_palindrome $n
+
+
